122b750ed70418273acfabbfbf6a4ff0d315ea2a
Сергей Федотов <fedser@lan.rarus.ru>
2024-07-01 17:00:05 +0300

    Реализация класса ParsedData и добавление пакета models. Импорт проекта.

---
+++ app/models/models.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+class ParsedData(BaseModel):
+    inn_kpp: str
+    invoice: str
+    data_table: list[list[str]]
+    total_without_tax: str
+    amount_of_tax: str
+    total_amount: str
+
